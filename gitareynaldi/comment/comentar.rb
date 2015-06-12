9f49b578c7d9d5a0bf5bb9bdf9229e59495e671e
gitarey <gitareynaldi11@gmail.com>
2015-06-12 21:47:27 +0700

    penambahan kometar

---
+++ gitareynaldi/comment/comentar.rb
@@ -0,0 +1,9 @@
+puts "komentar"
+
+
+
+#Anda dapat komentar beberapa baris menggunakan = mulai dan akhir = sintaks
+=begin
+This is a multiline comment and con spwan as many lines as you
+like. But =begin and =end should come in the first line only. 
+=end
