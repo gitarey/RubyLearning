ba7a63861ada4b71d30d87f0ddecaabe5fa04779
gitarey <gitareynaldi11@gmail.com>
2015-06-14 14:56:07 +0700

    penambahan komentar file I/O

--- gitareynaldi/file I.O/file_input.rb
+++ gitareynaldi/file I.O/file_input.rb
@@ -1,6 +1,6 @@
 aFile = File.new("input.txt", "r")     #membuka file dengan nama File
 if aFile
-   content = aFile.sysread(20)          #menampilkan 20 karakter awal dari file
+   content = aFile.sysread(10)          #menampilkan 10 karakter awal dari file
    puts content
 else
    puts "Unable to open file!"
