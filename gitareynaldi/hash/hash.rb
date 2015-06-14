7f6153eee40d5f11d852e2a21413d528c0fd38ae
gitarey <gitareynaldi11@gmail.com>
2015-06-14 13:59:16 +0700

    pemberian komentar

---
+++ gitareynaldi/hash/hash.rb
@@ -0,0 +1,9 @@
+$, = ", "
+months = Hash.new( "month" )                   #deklaasi hash
+
+months = {"1" => "January", "2" => "February", "3" => "Maret"} #pemberian key pada bulan
+
+keys = months.keys          # deklarasi mana yang akan menjadi key
+
+
+puts "#{keys}"   #output key
