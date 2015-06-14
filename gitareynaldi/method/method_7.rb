11f78528ca464d4bcb74d3b7b1573752ebe230a6
gitarey <gitareynaldi11@gmail.com>
2015-06-14 11:47:22 +0700

    pemberian komentar method

---
+++ gitareynaldi/method/method_7.rb
@@ -0,0 +1,6 @@
+def test(a1="Pecel", a2="Sate Ayam")       #pemberian variable
+   puts "daftar makanan #{a1}"				# output untuk variable
+   puts "daftar makanan #{a2}"
+end
+test "Kentang Goreng", "Nasi Goreng"
+test
