b40e763b534a047d34800c4ca887faba1166b99a
gitarey <gitareynaldi11@gmail.com>
2015-06-14 14:06:07 +0700

    penambahann komentar date_time

---
+++ gitareynaldi/date_time/date_time.rb
@@ -0,0 +1,7 @@
+time1 = Time.new
+
+puts "Current Time : " + time1.inspect     #mengambil time dari system operasi
+
+# Time.now is a synonym:
+time2 = Time.now
+puts "Current Time : " + time2.inspect
