892fdda474d2b89f8ea8673cf604de1a5365bb8c
gitarey <gitareynaldi11@gmail.com>
2015-06-14 14:25:54 +0700

    penambahan komentar range

---
+++ gitareynaldi/range/range.rb
@@ -0,0 +1,6 @@
+$, =", "   # Array value separator
+range1 = (1..10).to_a          #jarak pertama dimana di mulai dari 1-10
+range2 = ('bar'..'baz').to_a   #range 2 dimana huruf terakhir akan di urutkan sesuai abjat dimana huruf di mulai dari r dan berakhir d z
+
+puts "#{range1}"
+puts "#{range2}"
